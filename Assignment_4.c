b13ad818c352ce602c01d05d2bec396518cc39d2
Farhan <108405018+farhan-farhan@users.noreply.github.com>
2022-07-27 15:52:36 +0530

    Add files via upload

---
+++ Assignment_4.c
@@ -0,0 +1,104 @@
+// Assignment - 4 
+// Iterative Control Statements
+#include<stdio.h>
+
+// 1. Write a program to print MySirG 5 times on the screen
+/*int main(){
+    int i;
+    for(i=0;i<5;i++){
+        printf("MySirG\n");
+    }
+}*/
+
+// 2. Write a program to print the first 10 natural numbers.
+/*int main(){
+    int i;
+    for(i=1;i<=10;i++)
+    printf("%d  ",i);
+}*/
+// 3. Write a program to print the first 10 natural numbers in reverse order
+/*int main(){
+    int i;
+    for(i=10;i>=1;i--)
+    printf("%d  ",i);
+  return 0;  
+}*/
+// 4. Write a program to print the first 10 odd natural numbers
+/*int main(){
+    int i ;
+    for(i=1; i<=10;i++)
+    printf("%d  ",2*i-1);
+}*/
+// 5. Write a program to print the first 10 odd natural numbers in reverse order.
+/*int main(){
+    int i ;
+    for(i=10; i>=1;i--)
+    printf("%d  ",2*i-1);
+}*/
+// 6. Write a program to print the first 10 even natural numbers
+/*int main(){
+    int i ;
+    for(i=1; i<=10;i++)
+    printf("%d  ",2*i);
+}*/
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+/*int main(){
+    int i ;
+    for(i=10; i>=1;i--)
+    printf("%d  ",2*i);
+}*/
+// 8. Write a program to print squares of the first 10 natural numbers
+/*int main(){
+    int i ;
+    for(i=1; i<=10;i++)
+    printf("%d  ",i*i);
+}*/
+// 9. Write a program to print cubes of the first 10 natural numbers
+/*int main(){
+    int i ;
+    for(i=1; i<=10;i++)
+    printf("%d  ",i*i*i);
+}*/
+// 10. Write a program to print a table of 5
+/*int main(){
+    int i ,n;
+    printf("Enter the table name:");
+    scanf("%d",&n);
+    for(i=1; i<=10;i++)
+    printf("\n%d * %d = %d",n,i,n*i);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main(){
+
+int  n,i,sum=0;
+printf("Enter the value of N");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    sum=sum+i;
+    
+}
+printf("sum is %d",sum);
+}*/
